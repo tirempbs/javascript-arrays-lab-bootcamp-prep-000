88cd51c241c7bf41886d21588409a54b90d74d00
Tirem Bareno-Sosna <tirempbs@gmail.com>
2018-08-03 13:37:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions and code here
+kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var moreKittens = [...kittens, name];
+  return moreKittens;
+}

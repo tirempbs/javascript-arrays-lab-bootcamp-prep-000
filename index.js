d55a658a9aa0b37a9b7580e5cc845513f986f930
Tirem Bareno-Sosna <tirempbs@gmail.com>
2018-08-03 13:51:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var lessKittens = kittens.slice(-1);
+  var lessKittens = kittens.slice(0, kittens.length - 1);
   return lessKittens;
 }
 

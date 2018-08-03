fed51905bdc2d853686b2f3489c0a4edb68f89b2
Tirem Bareno-Sosna <tirempbs@gmail.com>
2018-08-03 13:38:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,12 @@ function appendKitten(name) {
   var moreKittens = [...kittens, name];
   return moreKittens;
 }
+
+function prependKitten(name) {
+  var moreKittens = [name, ...kittens];
+  return moreKittens;
+}
+
+function removeLastKitten() {
+  
+}
